f3693feadc9e305141e2e5af5225cc0213762389
Anna Carey <anna@annajcarey.com>
2019-11-18 21:31:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog 
-  def name = (dog_name)
-  this_dogs_name = dog_name
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end 
+
+  def name 
+    this_dogs_name 
+  end 
 end 
 
-def name 
-  this_dogs_name 
-end 
-end 
+lass = Dog.new 
+lassie.name = "Lassie"
+lassie.name
